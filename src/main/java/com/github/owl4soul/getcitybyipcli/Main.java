afb6607d330c40e2cc74ecb88bd6dd1e217e0a19
owl4soul <resuuser989@gmail.com>
2019-01-26 11:40:55 +0300

    минимальный проект с executable jar
    запускать либо командой в терминале
    mvn clean compile assembly:single
    
    либо через IDEA
    maven -> plugins -> assembly -> assembly:single

---
+++ src/main/java/com/github/owl4soul/getcitybyipcli/Main.java
@@ -0,0 +1,14 @@
+package com.github.owl4soul.getcitybyipcli;
+
+public class Main {
+
+    //todo
+    //
+
+    //todo убрать горячие клавишы из ubuntu которые исп-ся в IDEA (Ctrl+Shift+L)
+
+    public static void main(String[] args) {
+        System.out.println("i work");
+    }
+
+}
