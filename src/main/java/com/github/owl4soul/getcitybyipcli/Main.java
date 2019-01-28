041d95082be4abe33db0441f87b23a393d48ce6f
owl4soul <resuuser989@gmail.com>
2019-01-28 16:36:56 +0300

    проверка обработки get-запроса
    минимальный проект с executable jar
    
    запускать либо командой в терминале
    mvn clean compile assembly:single
    
    либо через IDEA
    maven -> plugins -> assembly -> assembly:single

--- src/main/java/com/github/owl4soul/getcitybyipcli/Main.java
+++ src/main/java/com/github/owl4soul/getcitybyipcli/Main.java
@@ -1,5 +1,11 @@
 package com.github.owl4soul.getcitybyipcli;
 
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class Main {
 
     //todo
@@ -7,8 +13,25 @@ public class Main {
 
     //todo убрать горячие клавишы из ubuntu которые исп-ся в IDEA (Ctrl+Shift+L)
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         System.out.println("i work");
+        String url = "http://ip-api.com/json/97.65.170.121";
+
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(response.toString());
     }
 
 }
